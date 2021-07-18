669e8d4c7ac1f8bceee11300cc430c9509ed7c5f
lethereal <58582863+lethereal@users.noreply.github.com>
2021-07-17 22:40:27 -0300

    Base pronta

--- js/script.js
+++ js/script.js
@@ -3,5 +3,25 @@ const form = document.getElementById('form');
 form.addEventListener('submit', (e) => 
 {
     e.preventDefault();
-    alert('Hello devs!')
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let data = 
+    {
+        nome,
+        email,
+    }
+    let convertData = JSON.stringify(data);
+    localStorage.setItem('lead', convertData)
+
+    let content = document.getElementById('content')
+    let carregando = `<p>Entregando seus dados para nosso office boy elfo...</p>`
+    let pronto = `<p>Dados salvos com sucesso!</p>`
+
+    content.innerHTML = carregando
+    setTimeout (() => 
+    {
+        content.innerHTML = pronto
+    }, 1000)
+
+    console.log(nome, email);
 })
